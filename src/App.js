79fbf3b711c29ecb52f81a21031f53716738efa1
wigiway <wigiway.studio@gmail.com>
2024-06-13 18:48:34 +0700

    Prepare for deploymentv11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const handleClick = () => {
+    window.location.href = 'https://qx58l.webar.run/45989794235815432/0.0.2/';
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="start-button" onClick={handleClick}>START</button>
     </div>
   );
-}
+};
 
 export default App;
