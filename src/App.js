71374a43fafdebd7d590465ffca3935d7205b19e
wigiway <152454955+wigiway@users.noreply.github.com>
2024-07-03 17:45:29 +0700

    Update App.js v11 v4

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const App = () => {
   const handleClick = () => {
-    window.location.href = 'https://vxg9g.webar.run/8237427486333416546/0.0.2/';
+    window.location.href = 'https://vxg9g.webar.run/8237427486333416546/0.0.4/';
   };
 
   return (
